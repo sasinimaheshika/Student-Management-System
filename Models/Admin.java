f2882997fa0265dde0105649b05cadaa6be2900d
sasinimaheshika <sasinimaheshika5@gmail.com>
2026-05-07 18:39:03 +0530

    change the variable name issue

--- Models/Admin.java
+++ Models/Admin.java
@@ -1,11 +1,11 @@
 package Models;
 
 public class Admin extends User {
-    private String employeeId;
+    private String employee_id;
 
-    public Admin(int userId, String username, String password, String employeeId) {
-        super(userId, username, password, "ADMIN");
-        this.employeeId = employeeId;
+    public Admin(int user_id, String username, String password, String employee_id) {
+        super(user_id, username, password, "ADMIN");
+        this.employee_id = employee_id;
     }
 
     @Override
@@ -15,7 +15,7 @@ public class Admin extends User {
         System.out.println("2. View All Students (READ)");
         System.out.println("3. Add New Course");
         System.out.println("4. Update Marks (UPDATE)");
-        System.out.println("5. Delete Student Record (DELETE)"); // Added for CRUD
+        System.out.println("5. Delete Student Record (DELETE)");
         System.out.println("6. Logout");
     }
 }
