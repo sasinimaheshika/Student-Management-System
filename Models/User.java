4d5cf255ebef718298f5a3b57246f623bd24c8ad
sasinimaheshika <sasinimaheshika5@gmail.com>
2026-05-08 13:09:18 +0530

    change the syntax errors

--- Models/User.java
+++ Models/User.java
@@ -1,20 +1,20 @@
 package Models;
 
 public abstract class User {
-    private int userId;
+    private int user_id;
     private String username;
     private String password;
     private String role;
 
-    public User(int userId, String username, String password, String role) {
-        this.userId = userId;
+    public User(int user_id, String username, String password, String role) {
+        this.user_id = user_id;
         this.username = username;
         this.password = password;
         this.role = role;
     }
 
     // Getters for Encapsulation[cite: 1, 8]
-    public int getUserId() { return userId; }
+    public int getUserId() { return user_id; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
     public String getRole() { return role; }
