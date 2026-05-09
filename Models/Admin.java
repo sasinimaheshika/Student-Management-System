83bef9fce49db2c9b8218f30cbf55c3a9f72d2ca
sasinimaheshika <sasinimaheshika5@gmail.com>
2026-05-09 13:56:03 +0530

    updated the interface of the admin panel

--- Models/Admin.java
+++ Models/Admin.java
@@ -1,21 +1,23 @@
 package Models;
 
 public class Admin extends User {
-    private String employeeId;
+    private String employee_id;
 
     public Admin(int userId, String username, String password, String employeeId) {
         super(userId, username, password, "ADMIN");
-        this.employeeId = employeeId;
+        this.employee_id = employeeId;
     }
 
     @Override
     public void displayMenu() {
-        System.out.println("\n--- Admin Menu ---");
-        System.out.println("1. Add Student (CREATE)");
-        System.out.println("2. View All Students (READ)");
+        System.out.println("\n --------------------");
+        System.out.println("|     Admin Menu     |");
+        System.out.println(" --------------------");
+        System.out.println("1. Add Student");
+        System.out.println("2. View All Students");
         System.out.println("3. Add New Course");
-        System.out.println("4. Update Marks (UPDATE)");
-        System.out.println("5. Delete Student Record (DELETE)"); // Added for CRUD
+        System.out.println("4. Update Marks");
+        System.out.println("5. Delete Student Record");
         System.out.println("6. Logout");
     }
 }
