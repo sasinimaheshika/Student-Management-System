ae3a2fe0ce755b1c1d5b8e646c452a077ec89238
sasinimaheshika <sasinimaheshika5@gmail.com>
2026-05-10 19:12:53 +0530

    change the student display menu

--- Models/Student.java
+++ Models/Student.java
@@ -5,17 +5,23 @@ public class Student extends User {
     private String email;
     private String phone;
 
-    public Student(int user_id, String username, String password, String name, String email, String phone) {
-        super(user_id, username, password, "STUDENT");
+    public Student(int userId, String username, String password, String name, String email, String phone) {
+        super(userId, username, password, "STUDENT");
         this.name = name;
         this.email = email;
         this.phone = phone;
     }
 
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+
     @Override
     public void displayMenu() {
-        System.out.println("\n--- Student Menu ---");
-        System.out.println("1. View My Results (READ)");
+        System.out.println("\n ------------------");
+        System.out.println("|   Student Menu   |");
+        System.out.println(" ------------------");
+        System.out.println("1. View My Results");
         System.out.println("2. View My Profile");
         System.out.println("3. Logout");
     }
