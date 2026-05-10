143460a8c897133062070e786e55903b5f9756b8
sasinimaheshika <sasinimaheshika5@gmail.com>
2026-05-10 19:11:05 +0530

    fix the  getters and setters issues in courses

--- Models/Course.java
+++ Models/Course.java
@@ -10,4 +10,14 @@ public class Course {
         this.courseName = courseName;
         this.courseCode = courseCode;
     }
+
+    // Getters for Encapsulation
+    public int getCourseId() { return courseId; }
+    public String getCourseName() { return courseName; }
+    public String getCourseCode() { return courseCode; }
+
+    // Setters
+    public void setCourseId(int courseId) { this.courseId = courseId; }
+    public void setCourseName(String courseName) { this.courseName = courseName; }
+    public void setCourseCode(String courseCode) { this.courseCode = courseCode; }
 }
