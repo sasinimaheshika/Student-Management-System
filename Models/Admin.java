84ac1393c7d7b8cf4bc2e0c572c87d1731866bc6
sasinimaheshika <sasinimaheshika5@gmail.com>
2026-05-10 19:10:13 +0530

    Add new CRUD operation

--- Models/Admin.java
+++ Models/Admin.java
@@ -1,23 +1,26 @@
 package Models;
 
 public class Admin extends User {
-    private String employee_id;
+    private String employeeId;
 
     public Admin(int userId, String username, String password, String employeeId) {
         super(userId, username, password, "ADMIN");
-        this.employee_id = employeeId;
+        this.employeeId = employeeId;
     }
+
+    public String getEmployeeId() { return employeeId; }
 
     @Override
     public void displayMenu() {
         System.out.println("\n --------------------");
         System.out.println("|     Admin Menu     |");
         System.out.println(" --------------------");
-        System.out.println("1. Add Student");
+        System.out.println("1. Add Student & Login");
         System.out.println("2. View All Students");
         System.out.println("3. Add New Course");
-        System.out.println("4. Update Marks");
-        System.out.println("5. Delete Student Record");
-        System.out.println("6. Logout");
+        System.out.println("4. View All Courses");
+        System.out.println("5. Update Marks");
+        System.out.println("6. Delete Student Record");
+        System.out.println("7. Logout");
     }
 }
